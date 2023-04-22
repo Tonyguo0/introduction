89d12016542dd7094f0f4a9455142f39a32de486
tonyguo0 <tonyguo998@gmail.com>
2023-04-22 23:54:19 +0800

    added hamburger button for the navbar if the website gets shrunk horizontally, added close and button toggle, added fully tailwind generated dropdown menu

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Portfolio page TBC...
+
+https://www.youtube.com/watch?v=0fYi8SGA20k&list=PLsEf0D1uqFXoCj5iBoEDFPPCunzfsnI3G&index=17&t=120s&ab_channel=JavaScriptMastery
+34:27
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,7 +8,7 @@ import { link } from 'fs'
 
 const Navbar = () => {
   const [active, setActive] = useState('')
-
+  const [toggle, setToggle] = useState(false)
   return (
     <nav
       className={`
@@ -32,16 +32,45 @@ const Navbar = () => {
               key={navLink.id}
               // making the navlink e.g. about work, contact have an active UI effect (turning white) when clicked and hovered over. Also set the title e.g. [About, Work, Contact] as the "active" state hook 
               className={`${active === navLink.title
-                  ? "text-white"
-                  : "text-secondary"
+                ? "text-white"
+                : "text-secondary"
                 } hover:text-white text-[18px]
               font-mdeium cursor-pointer`}
-              onClick={()=> setActive(navLink.title)}
+              onClick={() => setActive(navLink.title)}
             >
               <a href={`#${navLink.id}`}>{navLink.title}</a>
             </li>
           ))}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)} />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((navLink) => (
+                <li
+                  key={navLink.id}
+                  // making the navlink e.g. about work, contact have an active UI effect (turning white) when clicked and hovered over. Also set the title e.g. [About, Work, Contact] as the "active" state hook 
+                  className={`${active === navLink.title
+                    ? "text-white"
+                    : "text-secondary"
+                    } hover:text-white text-[18px]
+              font-mdeium cursor-pointer`}
+                  onClick={() => setActive(navLink.title)}
+                >
+                  <a href={`#${navLink.id}`}>{navLink.title}</a>
+                </li>
+              ))}
+            </ul>
+
+
+          </div>
+        </div>
       </div>
     </nav>
   )
