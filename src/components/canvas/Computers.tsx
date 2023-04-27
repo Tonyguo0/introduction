84db3cd4839f3fbc7451d624eddb91f172874964
tonyguo0 <tonyguo998@gmail.com>
2023-04-27 23:51:20 +0800

    added framer motion's little scroll hint animated pic, added halfway support on mobile application viewing of the website useEffect only right now

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -28,6 +28,25 @@ const Computers = () => {
 }
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('max-width: 500');
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event: { matches: boolean | ((prevState: boolean) => boolean) }) => {
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop="demand"
