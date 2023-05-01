d100ef04e4f18675e5d5b516ae22858f2d31cf1c
tonyguo0 <tonyguo998@gmail.com>
2023-05-01 23:55:59 +0800

    added Tilt and motion.div to serviceCard so it'll have nice animation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,11 +12,14 @@ interface propsType {
   icon: string
 }
 
-const ServiceCard = ({ index, title, icon }:propsType) => {
+const ServiceCard = ({ index, title, icon }: propsType) => {
   return (
-    <div>
-    <p>{title}</p>
-    </div>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+      </motion.div>
+    </Tilt>
   )
 }
 
