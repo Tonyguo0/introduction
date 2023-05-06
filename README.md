5002548ad690aba695f2b8d1e375ca464ecc95fa
tonyguo0 <tonyguo998@gmail.com>
2023-05-07 01:04:53 +0800

    added Section Wrapper and wrapped about section

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Portfolio page TBC...
 
 https://www.youtube.com/watch?v=0fYi8SGA20k&list=PLsEf0D1uqFXoCj5iBoEDFPPCunzfsnI3G&index=17&t=120s&ab_channel=JavaScriptMastery
-1:15:00
+1:18:30
 
 - Navbar Section Done
 - Hero Section Done
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
 
 interface propsType {
   index: number,
@@ -13,24 +14,24 @@ interface propsType {
 
 const ServiceCard = ({ index, title, icon }: propsType) => {
   return (
-    <Tilt className='parallax-effect xs:w-[250px] w-full' perspective={1000} scale={1.1} transitionSpeed={450} tiltMaxAngleX={35} tiltMaxAngleY={35} gyroscope={true}>
-      {/* <p className='inner-element'>wassup beijing</p> */}
+    <Tilt className='parallax-effect xs:w-[250px] w-full' perspective={1000} scale={1.1} transitionSpeed={450} tiltMaxAngleX={35} tiltMaxAngleY={35}>
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className=' w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        {/* options={{max: 45, scale: 1, speed: 450}} */}
+        className='inner-element w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
         <div
           className='inner-element bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
           <img src={icon} alt={title}
-            className=" inner-element w-16 h-16 object-contain" />
-          <h3 className='inner-element text-white text-[20px] flex-bold text-center'>
+            className="inner-inner-element w-16 h-16 object-contain" />
+          <h3 className='inner-inner-element text-white text-[20px] font-bold text-center'>
             {title}
           </h3>
 
 
         </div>
       </motion.div>
+
+
     </Tilt>
   )
 }
@@ -65,4 +66,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
