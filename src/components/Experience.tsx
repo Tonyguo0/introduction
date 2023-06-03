f6ed56cc396668a69a15d9c745cfb63a5a57e8f4
tonyguo0 <tonyguo998@gmail.com>
2023-06-03 22:40:17 +0800

    experience component completed

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -26,7 +26,7 @@ const ExperienceCard = ({ experience }: propsType) => (
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className='flex justify-center items-center w-full h-full'>
         <img
           src={experience.icon}
           alt={experience.company_name}
@@ -35,9 +35,21 @@ const ExperienceCard = ({ experience }: propsType) => (
     }
   >
     <div>
-      <h3></h3>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p className='text-secondary text-[16px] font-semibold' style={{ margin: 0 }}>{experience.company_name}</p>
     </div>
-    {experience.date}
+
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className='text-white-100 text-[14px] pl-1 tracking-wider'
+        >
+          {point}
+
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 )
 
