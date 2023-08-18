2ea36c6b297c197b9ff4cb8505c7c99a10a5041e
tonyguo0 <tonyguo998@gmail.com>
2023-08-18 08:50:15 +0800

    finished feedback section with some different styling with the -div and section wrapper added, added profile photo for the person's feedbacks, made a start on the contact section

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Portfolio page TBC...
 
 https://www.youtube.com/watch?v=0fYi8SGA20k&list=PLsEf0D1uqFXoCj5iBoEDFPPCunzfsnI3G&index=17&t=120s&ab_channel=JavaScriptMastery
-1:52:50
+2:15:10
 
 - Navbar Section Done
 - Hero Section Done
 - About Section Done
-- Experience Section Done
-- Technoloies & Balls Section Done
+- Works Section Done
+- Technologies & Balls Section Done
+- Projects Section Done
+- Feedback Section Done
+- Contact form In-Progress
 
 SketchFab
 Logo.com
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import {
+    About,
+    Contact,
+    Experience,
+    Feedbacks,
+    Hero,
+    Navbar,
+    Tech,
+    Works,
+    StarsCanvas,
+} from "./components";
 
 const App = () => {
-  return (
-    <BrowserRouter>
+    return (
+        <BrowserRouter>
+            <div className="relative z-0 bg-primary">
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar />
+                    <Hero />
+                    <About />
+                    <Experience />
+                    <Tech />
+                    <Works />
+                    <Feedbacks />
+                    <div className="relative z-0">
+                        <Contact />
+                        <StarsCanvas />
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+};
 
-      <div className='relative z-0 bg-primary'>
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-
-          <Navbar />
-          <Hero />
-          <About/>
-          <Experience/>
-          <Tech/>
-          <Works/>
-          <Feedbacks/>
-          <div className="relative z-0">
-            <Contact/>
-            <StarsCanvas/>
-          </div>
-
-        </div>
-
-      </div>
-    </BrowserRouter>
-  )
-}
-
-export default App
+export default App;
