3e00ee6cf0cd46f27413e09e3796607e9af2bfd9
tonyguo0 <tonyguo998@gmail.com>
2023-08-21 19:58:03 +0800

    Contact section in-progress have the border/ styling done with good flex to seperate the components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,37 @@
-import { useState, useRef } from "react"
-import { motion } from "framer-motion"
-import emailjs from "@emailjs/browser"
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
-import { styles } from "../styles"
-import { EarthCanvas } from "./canvas"
-import { SectionWrapper } from "../hoc"
-import { slideIn } from "../utils/motion"
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef()
-  const [form, setForm] = useState({name: '', email:'', message: ''})
-  const [loading, setLoading] = useState(false)
-  return (
-    <div>Contact</div>
-  )
-}
+		const formRef = useRef();
+		const [form, setForm] = useState({ name: "", email: "", message: "" });
+		const [loading, setLoading] = useState(false);
 
-export default Contact
+		const handlChange = (e) => {};
+
+		const handlSubmit = (e) => {};
+		return (
+				// classname to ensure we have everything we need to show in mobile, and larger devices
+				<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+						{/* 
+								flex-[0.75] = take 3/4 of the screen
+								
+				
+						*/}
+						<motion.div
+								variants={slideIn("left", "tween", 0.2, 1)}
+								className="flex-[0.75] bg-black p-8 rounded-2xl"
+						>
+							<p>Get in touch</p>
+							<h3>Contact.</h3>
+						</motion.div>
+				</div>
+		);
+};
+
+export default SectionWrapper(Contact, "contact");
