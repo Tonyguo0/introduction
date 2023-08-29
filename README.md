dfc36e150a063c079c73bbc15c99bdef622022f4
tonyguo0 <tonyguo998@gmail.com>
2023-08-29 22:12:03 +0800

    stars not working for some reason but the component is being worked on and almost finished

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Portfolio page TBC...
 
 https://www.youtube.com/watch?v=0fYi8SGA20k&list=PLsEf0D1uqFXoCj5iBoEDFPPCunzfsnI3G&index=17&t=120s&ab_channel=JavaScriptMastery
-2:31:50
+2:38:27
 
 - Navbar Section Done
 - Hero Section Done
--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useState, useRef, Suspense } from "react";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.cjs";
+// import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
+const Stars = (props) => {
+    const ref = useRef();
+    const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
-export default Stars
+    // to learn more go to threejs or react fiber and type stride there
+    // return (
+    //     <group rotation={[0, 0, Math.PI / 4]}>
+    //         <points
+    //             ref={ref}
+    //             positions={sphere}
+    //             stride={3}
+    //             frustumCulled
+    //             {...props}
+    //         />
+    //         <PointMaterial
+    //             transparent
+    //             color="#f272c8"
+    //             size={0.002}
+                
+    //         />
+    //     </group>
+    // );
+};
+
+const StarsCanvas = () => {
+    // absolute to show behind the actual earth and contact form
+    // z-[-1] to show behind those elements
+    return (
+        <div className="w-full h-full absolute inset-0 ">
+            <Canvas camera={{ position: [0, 0, 1] }}>
+                <Suspense fallback={null}>
+                    <Stars />
+                </Suspense>
+
+                <Preload all />
+            </Canvas>
+        </div>
+    );
+};
+
+export default StarsCanvas;
