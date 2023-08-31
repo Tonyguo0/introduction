2d3f0843dae19a61e7a09259fcf00d3d159c2fad
tonyguo0 <tonyguo998@gmail.com>
2023-08-31 22:30:21 +0800

    FIXED Stars canvas, wrote a self closing tag instead of closing tag... added rotation of stars with useFrame from react Fiber

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -8,31 +8,34 @@ const Stars = (props) => {
     const ref = useRef();
     const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
+    // delta = change
+    useFrame((state, delta) => {
+        ref.current.rotation.x -= delta / 10;
+        ref.current.rotation.y -= delta / 15;
+    });
+
     // to learn more go to threejs or react fiber and type stride there
-    // return (
-    //     <group rotation={[0, 0, Math.PI / 4]}>
-    //         <points
-    //             ref={ref}
-    //             positions={sphere}
-    //             stride={3}
-    //             frustumCulled
-    //             {...props}
-    //         />
-    //         <PointMaterial
-    //             transparent
-    //             color="#f272c8"
-    //             size={0.002}
-                
-    //         />
-    //     </group>
-    // );
+    // MAKE SURE to use closing tag and self closing tags properly
+    return (
+        <group rotation={[0, 0, Math.PI / 4]}>
+            <Points
+                ref={ref}
+                positions={sphere}
+                stride={3}
+                frustumCulled
+                {...props}
+            >
+                <PointMaterial transparent color="#f272c8" size={0.002} />
+            </Points>
+        </group>
+    );
 };
 
 const StarsCanvas = () => {
     // absolute to show behind the actual earth and contact form
     // z-[-1] to show behind those elements
     return (
-        <div className="w-full h-full absolute inset-0 ">
+        <div className="w-full h-auto absolute inset-0 ">
             <Canvas camera={{ position: [0, 0, 1] }}>
                 <Suspense fallback={null}>
                     <Stars />
