12dfbc5f77ac44ca575c8412862648869bf2be0e
tonyguo0 <tonyguo998@gmail.com>
2023-09-14 19:50:34 +0800

    trying to fix the relative path of home button in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link
-                    to=".."
+                    to="."
                     relative="path"
                     className="flex items-center gap-2"
                     onClick={() => {
